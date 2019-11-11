8753522369e0577680b16e625999be2225db03fd
yangy <yangyixiaofirst@163.com>
2019-11-11 19:16:51 +0800

    small change.

--- CachecaTest/src/cn/yyx/Evaluate.java
+++ CachecaTest/src/cn/yyx/Evaluate.java
@@ -1,9 +1,12 @@
 package cn.yyx;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 import edu.ucdavis.cacheca.CachecaComputer;
 import edu.ucdavis.cacheca.Word;
@@ -45,7 +48,7 @@ public class Evaluate {
 		return result;
 	}
 	
-	public ArrayList<Double> EvaluateAccuracy(String one_example) {
+	public Accuracy EvaluateAccuracy(String one_example) {
 		ArrayList<Double> result = new ArrayList<Double>();
 		for (int i=0;i<top_ks.length;i++) {
 			result.add(0.0);
@@ -60,10 +63,44 @@ public class Evaluate {
 				result.set(j, result.get(j) + as.get(j)*1.0);
 			}
 		}
+		return new Accuracy(result, oes.length*1.0);
+	}
+	
+	public ArrayList<Double> EvaluateAccuracy(File test_file) {
+		ArrayList<Double> result = new ArrayList<Double>();
+		for (int i=0;i<top_ks.length;i++) {
+			result.add(0.0);
+		}
+		double r_size = 0;
+		List<String> examples = new LinkedList<String>();
+		try {
+			examples.addAll(FileUtil.ReadFile(test_file));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		for (String example : examples) {
+			Accuracy acc = EvaluateAccuracy(example);
+			for (int j=0;j<top_ks.length;j++) {
+				result.set(j, result.get(j) + acc.top_k.get(j)*1.0);
+			}
+			r_size += acc.size;
+		}
 		for (int j=0;j<top_ks.length;j++) {
-			result.set(j, result.get(j)/(oes.length*1.0));
+			result.set(j, result.get(j)/r_size);
 		}
 		return result;
+	}
+	
+	class Accuracy {
+		
+		ArrayList<Double> top_k = new ArrayList<Double>();
+		double size = 0;
+		
+		public Accuracy(ArrayList<Double> top_k, double size) {
+			this.top_k.addAll(top_k);
+			this.size = size;
+		}
+		
 	}
 	
 }
---
+++ CachecaTest/src/cn/yyx/FileUtil.java
@@ -0,0 +1,30 @@
+package cn.yyx;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtil {
+	
+	public static List<String> ReadFile(File test_file) throws IOException {
+        List<String> list = new ArrayList<String>();
+        FileInputStream fis = new FileInputStream(test_file);
+        InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+        BufferedReader br = new BufferedReader(isr);
+        String line = "";
+        while ((line = br.readLine()) != null) {
+            if (line.lastIndexOf("---") < 0) {
+                list.add(line);
+            }
+        }
+        br.close();
+        isr.close();
+        fis.close();
+        return list;
+    }
+	
+}
